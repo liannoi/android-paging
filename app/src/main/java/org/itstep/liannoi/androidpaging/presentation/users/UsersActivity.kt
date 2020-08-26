482fa28d0aeabb63d19704d5c2bf3b5023dbdacc
Maksym Liannoi <liannoi444@gmail.com>
2020-08-26 07:45:39 +0300

    Prepared boilerplate

---
+++ app/src/main/java/org/itstep/liannoi/androidpaging/presentation/users/UsersActivity.kt
@@ -0,0 +1,13 @@
+package org.itstep.liannoi.androidpaging.presentation.users
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.itstep.liannoi.androidpaging.R
+
+class UsersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_users)
+    }
+}
